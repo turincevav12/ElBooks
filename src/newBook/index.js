a32763abae222369199951af66ceedf56b512e12
woddy <turincev_av12@mail.ru>
2018-04-27 08:46:37 +0500

    add form new book

--- src/newBook/index.js
+++ src/newBook/index.js
@@ -6,23 +6,53 @@ class NameNewBook extends Component {
     render() {
         return(
             <div className="formNameNewBook">
-                <span>Наименование: </span>
+                <div id="top-line-creat-book-name">
+                    <span>Название книги</span> 
+                </div>
                 <input id="newNameBook" placeholder="Наименование"></input>
 
                 <div id="buttons">
-                    <div onClick={Creat}>Создать</div>
-                    <div onClick={Cancel}>Отмена</div>
+                    <div onClick={Creat} className="buttons-creat-new-book">Создать</div>
+                    <div onClick={Cancel} className="buttons-creat-new-book">Отмена</div>
                 </div>
             </div>
         )
     }    
 }
 
+class NewBook extends Component {
+    
+    render(){
+        const NameBook = document.getElementById('newNameBook').value
+
+        return(
+            <div className="NewBook">
+                <div id="book">
+                    <div id="nameBook">
+                        <span>{NameBook}</span>
+                    </div>
+                </div>
+                <div id="newBookButtons">
+                    <div className="newBookButton" onClick={alert('1')}>
+                        <span>Создать обзац...</span>
+                    </div>
+                    <div className="newBookButton" onClick={alert('1')}>
+                        <span>Создать текс абзаца...</span>
+                    </div>
+                    <div className="newBookButton" onClick={alert('1')}>
+                        <span>Сохранить</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
 function Creat () {
-    alert('1')
+    render(<NewBook />,document.getElementById("form"))
 }
 
 function Cancel () {
-    alert('2')
+    render("",document.getElementById("form"))
 }
 export default NameNewBook
